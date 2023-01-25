9118291aa6c20886251544a34ddbdcce60d46fd3
Lois May Cezar <85215270+loismayc@users.noreply.github.com>
2023-01-25 08:41:06 +0800

    add specs of vehicle in user input

--- Program.cs
+++ Program.cs
@@ -2,58 +2,92 @@
 
 namespace CarFactory
 {
-    public class Program {
-        static void Main(string[] args){
+    public class Program
+    {
+        static void Main(string[] args)
+        {
             List<Vehicle> vehicles = new List<Vehicle>();
             Console.WriteLine("Enter number of vehicles: ");
             int input = int.Parse(Console.ReadLine());
 
-            Car car = new Car ("Black","Toyota","Car");
-            Truck truck = new Truck ("White","Toyota","Truck", 40.20f);
-            int i =0;
+            //Car car = new Car ("Black","Toyota","Car");
+            //Truck truck = new Truck ("White","Toyota","Truck", 40.20f);
+            int i = 0;
 
-                while(i < input){
-                    for ( i=0; i < input; i++){
+            while (i < input)
+            {
+                for (i = 0; i < input; i++)
+                {
                     Console.WriteLine("\nEnter which type of vehicle (Car/Truck): ");
                     string type = Console.ReadLine().ToLower();
-                    if (type == "car"){
-                       /* Console.WriteLine("Enter color");
+                    if (type == "car")
+                    {
+                        Console.Write("Enter color: ");
                         string color = Console.ReadLine();
 
-                        Console.WriteLine("Enter brand");
+                        Console.Write("Enter brand: ");
                         string brand = Console.ReadLine();
 
-                        Console.WriteLine("Enter model");
+                        Console.Write("Enter model: ");
                         string model = Console.ReadLine();
 
-                        Car car = new Car (color, brand, model);*/
-                   
+                        Car car = new Car(color, brand, model);
+
                         vehicles.Add(car);
-                    } else if (type == "truck"){
+                    }
+                    else if (type == "truck")
+                    {
+                        Console.Write("Enter color: ");
+                        string color = Console.ReadLine();
+
+                        Console.Write("Enter brand: ");
+                        string brand = Console.ReadLine();
+
+                        Console.Write("Enter model: ");
+                        string model = Console.ReadLine();
+
+                        Console.Write("Enter capacity: ");
+                        float capacity = float.Parse(Console.ReadLine());
+
+                        Truck truck = new Truck(color, brand, model, capacity);
+
                         vehicles.Add(truck);
-                    } else {
+                    }
+                    else
+                    {
                         Console.WriteLine("Invalid input. Please try again.");
-                    }  
+                    }
                 }
-               
+
             }
-            Console.WriteLine("\n===================\nList of Vehicles:\n");
+            Console.WriteLine("\n===================\nList of Vehicles:");
 
 
-            foreach (Vehicle v in vehicles){
+            foreach (Vehicle v in vehicles)
+            {
                 if (v.GetType() == typeof(Car))
                 {
                     // Typecasting 
                     Car temp = (Car)v;
-                    Console.WriteLine("Vehicle is a Car");
+
+                    Console.WriteLine("\nVehicle is a car:");
+                    Console.WriteLine("Color: " + v.Color);
+                    Console.WriteLine("Brand: " + v.Brand);
+                    Console.WriteLine("Model: " + ((Car)v).Model);
+
                 }
                 else if (v.GetType() == typeof(Truck))
                 {
                     Truck temp = (Truck)v;
-                    Console.WriteLine("Vehicle is a Truck");
+                    Console.WriteLine("\nVehicle is a truck:");
+                    Console.WriteLine("Color: " + v.Color);
+                    Console.WriteLine("Brand: " + v.Brand);
+                    Console.WriteLine("Model: " + ((Truck)v).Model);
+                    Console.WriteLine("Capacity: " + ((Truck)v).Capacity);
+
                 }
             }
-                                    
+
         }
     }
 }
